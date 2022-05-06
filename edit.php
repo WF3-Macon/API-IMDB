c4d0ec32625cd34917b80e12b1943835c85b8d06
Guillaume Bouyer <guillaume.bouyer@me.com>
2022-05-06 13:56:25 +0200

    Edit

--- edit.php
+++ edit.php
@@ -45,3 +45,75 @@ if (empty($_GET['id'])) {
 
 // Récupération de la valeur du paramètre "id"
 $id = htmlspecialchars(strip_tags($_GET['id']));
+
+// Récupère les données envoyées
+$datas = json_decode(file_get_contents('php://input'), true);
+
+/**
+ * Si tous les champs sont bien remplis, 
+ * on met à jour en BDD
+ */
+if (
+    !empty($datas['title']) 
+    && !empty($datas['description'])
+    && !empty($datas['date'])
+    && !empty($datas['time'])
+    && !empty($datas['director'])
+    && !empty($datas['image'])
+    && !empty($datas['trailer'])
+) {
+    // Nettoie les données
+    foreach ($datas as $key => $value) {
+        $datas[$key] = htmlspecialchars(strip_tags($value)); 
+    }
+
+    // Connexion à la BDD
+    require_once 'connexion.php'; 
+
+    // Met à jour en BDD
+    $query = $db->prepare('UPDATE movies SET title = :title, description = :description, date = :date, time = :time, director = :director, image = :image, trailer = :trailer WHERE id = :id');
+    $query->bindValue(':title', $datas['title']);
+    $query->bindValue(':description', $datas['description']);
+    $query->bindValue(':date', $datas['date']);
+    $query->bindValue(':time', $datas['time'], PDO::PARAM_INT);
+    $query->bindValue(':director', $datas['director']);
+    $query->bindValue(':image', $datas['image']);
+    $query->bindValue(':trailer', $datas['trailer']);
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    $query->execute();
+
+    /**
+     * Si aucune ligne de la table SQL n'est affectée par la
+     * requête ci-dessus, on retourne une erreur
+     */
+    if ($query->rowCount() === 0) {
+        http_response_code(404);
+
+        echo json_encode([
+            'status' => 404,
+            'message' => 'Not Found'
+        ]);
+    
+        exit;
+    }
+
+    // 200 - OK
+    http_response_code(200);
+
+    // Retour de la ressource
+    echo json_encode([
+        'id' => $id,
+        ...$datas
+    ]);
+}
+// Sinon on retourne une erreur...
+else {
+    http_response_code(400);
+
+    echo json_encode([
+        'status' => 400,
+        'message' => 'Bad Request'
+    ]);
+
+    exit;
+}
