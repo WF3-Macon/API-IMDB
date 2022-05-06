bdec652558ddb63c3a6180ae04f2339bc567ed82
Guillaume Bouyer <guillaume.bouyer@me.com>
2022-05-06 12:09:35 +0200

    Update add

--- add.php
+++ add.php
@@ -31,11 +31,6 @@ if ($method !== 'POST') {
 // Récupère les données envoyées en POST
 $datas = json_decode(file_get_contents('php://input'), true);
 
-// Nettoie les données
-foreach ($datas as $key => $value) {
-    $datas[$key] = htmlspecialchars(strip_tags($value)); 
-}
-
 /**
  * Si tous les champs sont bien remplis, on insère en BDD
  */
@@ -48,7 +43,37 @@ if (
     && !empty($datas['image'])
     && !empty($datas['trailer'])
 ) {
-    echo json_encode($datas);
+    // Nettoie les données
+    foreach ($datas as $key => $value) {
+        $datas[$key] = htmlspecialchars(strip_tags($value)); 
+    }
+
+    // Connexion à la BDD
+    require_once 'connexion.php'; 
+
+    // Insertion en BDD
+    $query = $db->prepare('INSERT INTO movies (title, description, date, time, director, image, trailer) VALUES (:title, :description, :date, :time, :director, :image, :trailer)');
+    $query->bindValue(':title', $datas['title']);
+    $query->bindValue(':description', $datas['description']);
+    $query->bindValue(':date', $datas['date']);
+    $query->bindValue(':time', $datas['time'], PDO::PARAM_INT);
+    $query->bindValue(':director', $datas['director']);
+    $query->bindValue(':image', $datas['image']);
+    $query->bindValue(':trailer', $datas['trailer']);
+    $query->execute();
+
+    // Récupération de l'ID nouvellement inséré
+    // https://www.php.net/manual/fr/pdo.lastinsertid.php
+    $id = $db->lastInsertId();
+
+    // 201 - Created
+    http_response_code(201);
+
+    // Retour de la ressource
+    echo json_encode([
+        'id' => $id,
+        ...$datas
+    ]);
 }
 // Sinon on retourne une erreur...
 else {
